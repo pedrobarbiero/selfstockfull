18a8e0f2e0c8ccd240dc03e3aeced052464204d3
Pedro Henrique Barbiero <pedrohenriquebarbiero@hotmail.com>
2019-04-04 21:34:39 -0400

    retirando as permissoes

--- config/dev.exs
+++ config/dev.exs
@@ -69,7 +69,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Configure your database
 config :self, Self.Repo,
   username: "postgres",
-  password: "admin",
+  password: "postgres",
   database: "self_dev",
   hostname: "localhost",
   pool_size: 10
--- lib/self_web/controllers/plugs/require_auth.ex
+++ lib/self_web/controllers/plugs/require_auth.ex
@@ -6,13 +6,14 @@ defmodule SelfWeb.Plug.RequireAuth do
   end
 
   def call(conn, _params) do
-    if (conn.assigns[:user]) do
-      conn
-    else
-      conn
-      |> Phoenix.Controller.put_flash(:error, "Sem permissão")
-      |> redirect(to: "/")
-      |> halt()
-    end
+    conn
+    # if (conn.assigns[:user]) do
+    #   conn
+    # else
+    #   conn
+    #   |> Phoenix.Controller.put_flash(:error, "Sem permissão")
+    #   |> redirect(to: "/")
+    #   |> halt()
+    # end
   end
 end
