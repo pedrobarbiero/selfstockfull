5216b2ffc8debe321e1c49a164370d9abd88a437
ebasantos <eriksantoz@icloud.com>
2019-04-08 14:05:29 -0300

    deployng css

--- config/dev.exs
+++ config/dev.exs
@@ -69,7 +69,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Configure your database
 config :self, Self.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   database: "self_dev",
   hostname: "localhost",
   pool_size: 10
